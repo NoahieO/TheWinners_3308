d19f1f90ea76cf16eea06086c00e70fc8326acdb
Noah Osteroos <noahosteroos@icloud.com>
2025-04-03 11:54:57 -0600

    much smaller example of inserts and create files

---
+++ init_data/insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO users (username, balance) VALUES (Johan, 29.52); 
+INSERT INTO users (username, balance) VALUES (William, 2500);
+INSERT INTO users (username, balance) VALUES (TwitterMan, 340);
+INSERT INTO users (username, balance) VALUES (Groblin, 5670);
+INSERT INTO users (username, balance) VALUES (Twoofer, 2.34);
+INSERT INTO users (username, balance) VALUES (MANMAN, 200000.01);
+
+INSERT INTO friends (user_id, friend_id) VALUES (1, 3);
+INSERT INTO friends (user_id, friend_id) VALUES (1, 4);
+INSERT INTO friends (user_id, friend_id) VALUES (3, 1);
+INSERT INTO friends (user_id, friend_id) VALUES (4, 1);
+INSERT INTO friends (user_id, friend_id) VALUES (4, 2);
+INSERT INTO friends (user_id, friend_id) VALUES (4, )
