cd1ac2676eaf5c86acd634e78e4d698773ec5bc0
dbakula-1 <daba3586@colorado.edu>
2025-04-24 00:09:33 -0600

    added more info

--- alter_bets_table.js
+++ alter_bets_table.js
@@ -2,8 +2,12 @@ const pgp = require('pg-promise')();
 
 const db = pgp({
     connectionString: 'postgresql://exampleuser:G3+Md427TNwB6Np9wUcNLM82EPX9fP8JMT@dpg-db0onn445d6rc7achtnq-a.oregon-postgres.render.com:5432/eampledb',
-  ssl: { rejectUnauthorized: false }
-});
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  });
+  
 
 (async () => {
   try {
