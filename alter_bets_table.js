e8f99a0ad6d36cfcc96011d738f9dd0eb699dd76
dbakula-1 <daba3586@colorado.edu>
2025-04-24 00:05:24 -0600

    added more info to alter bets

--- alter_bets_table.js
+++ alter_bets_table.js
@@ -1,7 +1,7 @@
 const pgp = require('pg-promise')();
 
 const db = pgp({
-  connectionString: 'postgresql://exampleuser:G3+Md427TNwB6Np9wUcNLM82EPX9fP8JMT@dpg-db0onn445d6rc7achtnq-a.oregon-postgres.render.com/users_db',
+    connectionString: 'postgresql://exampleuser:G3+Md427TNwB6Np9wUcNLM82EPX9fP8JMT@dpg-db0onn445d6rc7achtnq-a.oregon-postgres.render.com:5432/exampledb',
   ssl: { rejectUnauthorized: false }
 });
 
